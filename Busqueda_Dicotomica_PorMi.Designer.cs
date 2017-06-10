1d1f100c60e96d6d179c44ff5124dd080a8193ba
Franco-NB <elpiole@gmail.com>
2017-06-10 18:24:57 -0300

    asd

--- Busqueda_Dicotomica_PorMi.Designer.cs
+++ Busqueda_Dicotomica_PorMi.Designer.cs
@@ -29,29 +29,33 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 12);
+            this.textBox1.Location = new System.Drawing.Point(162, 40);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(44, 20);
             this.textBox1.TabIndex = 0;
             // 
             // listBox1
             // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(13, 40);
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(125, 251);
+            this.listBox1.Size = new System.Drawing.Size(123, 290);
             this.listBox1.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(63, 9);
+            this.button1.Location = new System.Drawing.Point(212, 37);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(52, 23);
             this.button1.TabIndex = 3;
             this.button1.Text = "Agregar";
             this.button1.UseVisualStyleBackColor = true;
@@ -59,16 +63,16 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(174, 72);
+            this.textBox2.Location = new System.Drawing.Point(162, 111);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(75, 20);
+            this.textBox2.Size = new System.Drawing.Size(44, 20);
             this.textBox2.TabIndex = 4;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(174, 118);
+            this.button2.Location = new System.Drawing.Point(212, 111);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(52, 23);
             this.button2.TabIndex = 5;
             this.button2.Text = "Buscar";
             this.button2.UseVisualStyleBackColor = true;
@@ -76,32 +80,54 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(174, 188);
+            this.textBox3.Location = new System.Drawing.Point(162, 180);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(75, 20);
+            this.textBox3.Size = new System.Drawing.Size(102, 20);
             this.textBox3.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(159, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Resultado en Indice:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(159, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Numero a Encontrar:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(159, 172);
+            this.label1.Location = new System.Drawing.Point(159, 12);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(105, 13);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "Resultado en Indice:";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Numero de Elementos";
             // 
             // Busqueda_Dicotomica_PorMi
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(290, 306);
+            this.ClientSize = new System.Drawing.Size(281, 312);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Busqueda_Dicotomica_PorMi";
             this.Text = "Busqueda_Dicotomica_PorMi";
             this.Load += new System.EventHandler(this.Busqueda_Dicotomica_PorMi_Load);
@@ -118,6 +144,8 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
     }
 }
--- Busqueda_Dicotomica_PorMi.cs
+++ Busqueda_Dicotomica_PorMi.cs
@@ -12,25 +12,42 @@ namespace Gestion {
         public Busqueda_Dicotomica_PorMi() {
             InitializeComponent();
         }
-
         private void Busqueda_Dicotomica_PorMi_Load(object sender, EventArgs e) {
 
+        }
+
+        int randomLimit = 1000;
+
+        //Cetralized place for error messages prompts
+        void error_prompt(int errorcode) {
+            switch (errorcode) {
+                case 1:
+                    MessageBox.Show("Ingrese un numero de elementos");
+                    break;
+                case 2:
+                    MessageBox.Show("Debe Ingresar un valor numerico");
+                    break;
+                case 3:
+                    MessageBox.Show("El numero Solicitado no esta en la lista!!");
+                    break;
+            }
         }
 
         private void button1_Click(object sender, EventArgs e) {
             listBox1.Items.Clear();
             int cantidad;
-            if (int.TryParse(textBox1.Text, out cantidad) == false)
-                MessageBox.Show("Ingrese un numero de elementos");
+            if (int.TryParse(textBox1.Text, out cantidad) == false) {
+                error_prompt(1);
+            }
             else {
                 Random R = new Random();
                 int[] d = new int[cantidad];
                 for (int i = 0; i < cantidad; i++) {
-                    d[i] = R.Next(1, 100);
+                    d[i] = R.Next(1, randomLimit);
                 }
                 int aux;
-                for (int i = 0; i <= cantidad -2; i++) { // el primero va a ir desde 0 a 18, osea el anteultimo numero.
-                    for (int j = i + 1; j <= cantidad -1; j++) { // el segundo va a ir de 1 a 19, osea del segundo al ultimo.
+                for (int i = 0; i <= cantidad - 2; i++) { // el primero va a ir desde 0 a 18, osea el anteultimo numero.
+                    for (int j = i + 1; j <= cantidad - 1; j++) { // el segundo va a ir de 1 a 19, osea del segundo al ultimo.
                         if (d[i] > d[j]) { //si el valor del primer index es mayor que el del segundo. 
 
                             aux = d[i];   // Guardas el primero en aux,  
@@ -47,9 +64,9 @@ namespace Gestion {
 
         //make a 2d array with indexes and values of a listbox
         private int[,] lstbox_to_array(ListBox lst) {
-            int[,] array = new int[lst.Items.Count,2];
+            int[,] array = new int[lst.Items.Count, 2];
             for (int i = 0; i < lst.Items.Count; i++) {
-                array[i,0] = i;
+                array[i, 0] = i;
                 array[i, 1] = Convert.ToInt32(lst.Items[i]);
             }
             return array;
@@ -57,16 +74,17 @@ namespace Gestion {
         private void button2_Click(object sender, EventArgs e) {
             textBox3.Text = "";
             int[,] arr = lstbox_to_array(listBox1);
-            int[] sbounds = new int[2] { 0, listBox1.Items.Count}; //Stores the bounds of the search.
+            int[] sbounds = new int[2] { 0, listBox1.Items.Count }; //Stores the bounds of the search.
             int middle; //for storing midpoint of searchbounds
             int val; //for storing the value of midpoint 
             int querry; //Number searched.
             int resultindex = 0; //for storing the index of the result
             bool found = false; //inficates if the search has finished.
 
-            if (int.TryParse(textBox2.Text, out querry) == false) {
-                MessageBox.Show("Debe Ingresar un valor numerico");
-            }
+            if (int.TryParse(textBox2.Text, out querry) == false)
+                error_prompt(2);
+            else if (querry >= randomLimit)
+                error_prompt(3);
             else {
                 while (found == false && sbounds[0] <= sbounds[1]) {
                     middle = (sbounds[0] + sbounds[1]) / 2;
@@ -81,7 +99,7 @@ namespace Gestion {
                         sbounds[0] = middle + 1;
                 }
                 if (found == false) {
-                    MessageBox.Show("El numero Solicitado no esta en la lista!!");
+                    error_prompt(3);
                 }
                 else {
                     textBox3.Text = resultindex.ToString();
@@ -91,4 +109,10 @@ namespace Gestion {
         }
     }
 }
+
+
+
+
+
+
 
